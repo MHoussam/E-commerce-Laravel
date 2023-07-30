278299a70d0fea0169f988a8cbccebfeb838b808
MHoussam <mohammadosta1999@gmail.com>
2023-07-30 07:34:27 +0300

    Front-end <> Added the products cards to the dashboard.

--- e-commerce-frontend/src/js/script.js
+++ e-commerce-frontend/src/js/script.js
@@ -34,6 +34,15 @@ pages.page_register = async (event) => {
   });
 }
 
+pages.page_dashboard = async (event) => {
+    event.preventDefault();
+    const dashboard_url = pages.base_url + "dashboard"
+    const response = await pages.dashboard(dashboard_url, event)
+    //console.log(response.data[0].first_name)
+    //event.preventDefault();
+    //const first_name = response.data[0];
+}
+
 pages.login = async (url,event) => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
@@ -103,6 +112,69 @@ pages.login = async (url,event) => {
     //     console.error("Error:", error);
     //   });
   }
+
+pages.register = async (url) => {
+  // const email = document.getElementById("email").value;
+  // const password = document.getElementById("password").value;
+  //event.preventDefault();
+  const first_name = document.getElementById("first_name").value;
+  const last_name = document.getElementById("last_name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  event.preventDefault();
+  localStorage.setItem('id', null);
+  localStorage.setItem('first_name', null);
+
+    try{
+      console.log('whatttt')
+
+      const userData = {
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: password
+      };
+
+      console.log(url)
+        response = await axios.post(
+          url,
+          userData
+          ).then(response => {
+            // Handle the response data from the server
+            console.log(response.data);
+
+            console.log('aaaa: ' + response.data[0])
+            //console.log('hmmmm: ' + response.data[1])
+            if(response.data[0] != 0) {
+              localStorage.setItem('id', response.data[1].id);
+              localStorage.setItem('first_name', response.data[1].first_name);
+              //console.log('re: ' + response.data[1])
+              console.log(response.data[1].first_name)
+              window.location.href = "./dashboard.html";
+            } else {
+              console.log("Fill all the inputs!");
+            }
+          })
+        
+        
+        //api_url = pages.base_url + "dashboard/" + response.data[0].email;
+
+        // try{
+        //   await axios.post(
+        //     api_url
+        //   );
+
+        //   
+        // }catch(error){
+        //   pages.print_message("Error from Linking (POST)" + error)
+        // }
+        //document.getElementById("title").innerHTML = "Hello " + response.data[0].first_name;
+        //console.log(response.data[0].first_name)
+    }catch(error){
+      console.log("Error from Register API: " + error)
+    }
+}
 
 pages.register = async (url) => {
   // const email = document.getElementById("email").value;
